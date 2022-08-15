7ee1f1550c2814a02e791684dbe2838df7bc7d96
debanjana-a11y <89571066+debanjana-a11y@users.noreply.github.com>
2022-08-16 00:16:01 +0530

    Adds HTML and CSS ongoing

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// const Book = {
-//     title: "Pride and Prejudice",
-//     author: "Jane Austin",
-//     pages: 150,
-//     read: false
-// };
+let myLibrary = [];
 
 function Book (title, author, pages, read) {
     this.title = title;
@@ -11,8 +6,47 @@ function Book (title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function() { 
-        return this.title + ", " + this.author + " "+ this.pages + " " + this.read;
+        return `${this.title}, ${this.author}, ${this.pages}, ${(this.read == true)? "Read": "Not read"}`;
     };
 }
 
-const book = new Book("Pride and Prejudice", "Jane Austin", 150, false);
+Book.prototype.changeStat = function() {
+    this.read = !this.read;
+};
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function removeBookFromLibrary(index) {
+    if (index > myLibrary.length - 1) {
+        console.log("No such book to delete");
+        return;
+    }
+    myLibrary.splice(index, 1);
+}
+
+function changeReadStatus(index) {
+    if (index > myLibrary.length - 1) {
+        console.log("No such book to delete");
+        return;
+    }
+    const book = myLibrary[index];
+    book.changeStat();
+}
+
+const book1 = new Book('Pride and Prejudice', 'Jane Austin', 273, true);
+const book2 = new Book('Little Women', 'Louisa May Alcott', 200, false);
+const book3 = new Book('Great Expectations', 'Charles Dickens', 300, true);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+console.log(myLibrary.map(e => e.info()));
+removeBookFromLibrary(0);
+console.log(myLibrary.map(e => e.info()));
+
+
+changeReadStatus(0);
+console.log(myLibrary.map(e => e.info()));
